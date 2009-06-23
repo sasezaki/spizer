aa32ae96797a6971923b7abaef20ebe70d6a9b1a
kazusuke sasezaki <sasezaki@gmail.com>
2009-06-23 23:20:38 +0900

    add Kumo_Handler_RegexMatch

---
+++ lib/Kumo/Handler/RegexMatch.php
@@ -0,0 +1,20 @@
+<?php
+//http://svn.openpear.org/Diggin_Scraper_Adapter_Htmlscraping/trunk/library/Diggin/Http/Response/Encoding.php
+require_once 'Diggin/Http/Response/Encoding.php';
+class Kumo_Handler_RegexMatch extends Spizer_Handler_RegexMatch
+{
+    public function handle(Spizer_Document $document)
+    {
+        //get! encoding_to_utf-8
+        $body = Diggin_Http_Response_Encoding::encode($document->getBody(), $document->getHeader('content-type'));
+        
+        if (preg_match($this->config['match'], $body, $m, PREG_OFFSET_CAPTURE)) {
+            $this->engine->log('RegexMatch', array(
+                'message' => 'Document body matched lookup expression',
+              'regex' => $this->config['match'],
+                'match' => $m[0][0],
+                'offset' => $m[0][1]
+            ));
+        }
+    }
+}
