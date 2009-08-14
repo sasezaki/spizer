a9732ee30b5c32e199abeca3b15bed860b5a932b
kazusuke sasezaki <sasezaki@gmail.com>
2009-08-14 22:33:35 +0900

    unset - transef-encoding

--- lib/Kumo/Handler/ScrapeAndRequestSender.php
+++ lib/Kumo/Handler/ScrapeAndRequestSender.php
@@ -14,11 +14,11 @@ class Kumo_Handler_ScrapeAndRequestSender extends Kumo_Handler_RequestMessageQue
 
         $process = new Diggin_Scraper_Process();
         $process->setExpression($config['expression']);
-        //$process->setName($config['name']);
         $process->setName('kumo');
-        $process->setArrayFlag((boolean) true);
-        //$process->setType($config['type']);
+        $process->setArrayFlag(isset($config['arrayflag']) ? (boolean)$config['arrayflag']: true);
         $process->setType(isset($config['type']) ? $config['type']: 'TEXT');
+        // use only first filter
+        $process->setFilters(isset($config['filters']) ? array_shift($config['filters']) : false);
 
         $this->scraper = new Diggin_Scraper();
         $this->scraper->process($process);
@@ -30,13 +30,17 @@ class Kumo_Handler_ScrapeAndRequestSender extends Kumo_Handler_RequestMessageQue
         if (!$doc instanceof Spizer_Document_Html) return;
 
         $headers = $doc->getAllHeaders();
-        unset($headers['content-encoding']);
+
+        //response is already decoded.
+        //if () {
+            unset($headers['transfer-encoding']);
+            unset($headers['content-encoding']);
+        //}
 
         $results = $this->scraper->scrape(new Zend_Http_Response($doc->getStatus(),
                                                       $headers,
                                                       $doc->getBody())
                                , $doc->getUrl());
-
 
         foreach ($results['kumo'] as $src) {
             $this->send($src);
