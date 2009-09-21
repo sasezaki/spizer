fd30892f216172089a952fff9994e85b7d35294a
kazusuke sasezaki <sasezaki+github@gmail.com>
2009-09-21 19:52:57 +0900

    add Kumo_Engine for handling quick-run Zend_Queue

--- lib/Kumo/Engine.php
+++ lib/Kumo/Engine.php
@@ -6,51 +6,31 @@ require_once 'Spizer/Engine.php';
 
 class Kumo_Engine extends Spizer_Engine
 {
-    protected $_zfQueueCustom;
-   
+
     /**
-     * Create a new Spizer Engine object
+     * some borrowd  Spizer_Engine->run
      *
-     * @param array $config Configuration array
+     * Run the crawler 
+     *
+     * @param Kumo_Request_MessageQueue(Zend_Queue ext)
      */
-    public function __construct(array $config = array(), $zfQueueCustomConfig = null)
+    public function runQueue(Kumo_Request_MessageQueue $queue, $receive = 100, $timeout = 1)  
     {
-        parent::__construct($config);
 
-        $zfQueueCustomConfig = array_merge(array(
-                                                'adapter' => 'Array',
-                                                'options' =>  array('name' => 'spizer')
-                                           ),
-                                           $zfQueueCustomConfig);
+        $messages = $queue->receive($receive, $timeout);
+        
+        // Set the default logger if not already set
+        if (! $this->logger) {
+            require_once 'Spizer/Logger/Xml.php';
+            $this->logger = new Spizer_Logger_Xml();
+        }
 
-        $this->_zfQueueCustom = new Kumo_ZFQueueCustom($zfQueueCustomconfig['adapter'], ['options']);
-    }
+        // Go!
+        foreach($messages as $count => $message) {
 
-    /**
-     * override Spizer_Engine
-     *
-     * Run the crawler until we hit the last URL
-     *
-     * @param string $url URL to start crawling from
-     */
-    public function run($url)
-	{
-		if (! Zend_Uri_Http::check($url)) {
-			require_once 'Spizer/Exception.php';
-			throw new Spizer_Exception("'$url' is not a valid HTTP URI");
-		}
-		$this->baseUri = Zend_Uri::factory($url);
-		$this->queue->append($url);
-		
-		// Set the default logger if not already set
-		if (! $this->logger) {
-		    require_once 'Spizer/Logger/Xml.php';
-		    $this->logger = new Spizer_Logger_Xml();
-		}
-		
-		// Go!
-		while (($request = $this->queue->next())) {
-		    $this->logger->startPage();
+            $request = $message->getBody();
+
+            $this->logger->startPage();
             $this->logger->logRequest($request);
     
             // Prepare HTTP client for next request
@@ -64,24 +44,24 @@ class Kumo_Engine extends Spizer_Engine
             try {
                 $response = new Spizer_Response($this->httpClient->request());
             } catch (Zend_Exception $e) {
-       	        fwrite(STDERR, "Error executing request: {$e->getMessage()}.\n");
-       	        fwrite(STDERR, "Request information:\n");
-       	        fwrite(STDERR, "  {$request->getMethod()} {$request->getUri()}\n");
-       	        fwrite(STDERR, "  Referred  by: {$request->getReferrer()}\n");
-       	    }
-       	    
-    	    $this->logger->logResponse($response);
-	    
-	        // Call handlers
-	        $this->callHandlers($request, $response);
-	    
-    	    // End page
-	        $this->logger->endPage();
-	        ++$this->requestCounter;
-	    
-    	    // Wait if a delay was set
-	    	if (isset($this->config['delay'])) sleep($this->config['delay']);
-		}
-	}
+                fwrite(STDERR, "Error executing request: {$e->getMessage()}.\n");
+                fwrite(STDERR, "Request information:\n");
+                fwrite(STDERR, "  {$request->getMethod()} {$request->getUri()}\n");
+                fwrite(STDERR, "  Referred  by: {$request->getReferrer()}\n");
+            }
+            
+            $this->logger->logResponse($response);
+        
+            // Call handlers
+            $this->callHandlers($request, $response);
+        
+            // End page
+            $this->logger->endPage();
+            ++$this->requestCounter;
+        
+            // Wait if a delay was set
+            if (isset($this->config['delay'])) sleep($this->config['delay']);
+        }
+    }
 }
 
