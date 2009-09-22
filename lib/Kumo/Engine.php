906ea86a6745036aeeaa6f7701be5903529ede36
kazusuke sasezaki <sasezaki+github@gmail.com>
2009-09-22 18:32:16 +0900

    using ProgressBar

--- lib/Kumo/Engine.php
+++ lib/Kumo/Engine.php
@@ -7,17 +7,42 @@ require_once 'Spizer/Engine.php';
 class Kumo_Engine extends Spizer_Engine
 {
 
+    /** Kumo_Request_MessageQueue */
+    protected $requestQueue = null;
+    /** Zend_ProgressBar */
+    protected $progressBar = null;
+
+    // @param boolean
+    public $deleteMessage = false;
+
+    public function setRequestQueue(Kumo_Request_MessageQueue $queue)
+    {
+        $this->requestQueue = $queue;
+    }
+
+    public function setProgressBar(Zend_ProgressBar $progressBar)
+    {
+        $this->progressBar = $progressBar;
+    }
+
+    public function getProgressBar()
+    {
+        if (!$this->progressBar) {
+            //require_once 'Zend/ProgressBar.php';
+            //$this->progressBar
+        }
+
+        return $this->progressBar;
+    }
+
     /**
      * some borrowd  Spizer_Engine->run
      *
      * Run the crawler 
      *
-     * @param Kumo_Request_MessageQueue(Zend_Queue ext)
      */
-    public function runQueue(Kumo_Request_MessageQueue $queue, $receive = 100, $timeout = 1)  
+    public function runQueue($receive = 100, $timeout = 1)  
     {
-
-        $messages = $queue->receive($receive, $timeout);
         
         // Set the default logger if not already set
         if (! $this->logger) {
@@ -25,6 +50,14 @@ class Kumo_Engine extends Spizer_Engine
             $this->logger = new Spizer_Logger_Xml();
         }
 
+        $messages = $this->requestQueue->receive($receive, $timeout);
+
+        $this->runMessages($messages);
+        if($this->progressBar) $this->progressBar->finish();
+    }
+
+    protected function runMessages($messages)
+    {
         // Go!
         foreach($messages as $count => $message) {
 
@@ -58,10 +91,31 @@ class Kumo_Engine extends Spizer_Engine
             // End page
             $this->logger->endPage();
             ++$this->requestCounter;
-        
+            if ($this->progressBar) $this->progressBar->update($this->requestCounter);
+            if ($this->deleteMessage) $this->requestQueue->deleteMessage($message);
+
             // Wait if a delay was set
             if (isset($this->config['delay'])) sleep($this->config['delay']);
         }
     }
+
+    /**
+     * 
+     * $messages = $queue->receive();
+     * $engine->startlogger();
+     * foreach() {
+     *     $engine->runMessage();
+     * }
+     *
+     */
+    // @todo not implemented, yet.
+    // for messages-loop handle
+    /*
+    public function runMessage($message)
+    {
+        //$request =;
+        //
+    }
+    */
 }
 
