bb3aed203371ae1869dae51d5fc329ebd41c1e04
kazusuke sasezaki <sasezaki+github@gmail.com>
2009-09-22 18:31:38 +0900

    add Debug echo content-type

--- lib/Kumo/Handler/Debug.php
+++ lib/Kumo/Handler/Debug.php
@@ -7,7 +7,7 @@ class Kumo_Handler_Debug extends Spizer_Handler_Abstract
     public function handle(Spizer_Document $doc)
     {
         if (isset($this->config['do']) && $this->config['do']) {
-            echo (string)$doc->getUrl();
+            echo (string)$doc->getUrl(), $doc->getHeader('content-type');
         }
     }
     
